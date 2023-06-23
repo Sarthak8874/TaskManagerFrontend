c39eb1fd7ebfe6a49e133019b687cebf9fcd0bc9
Sarthak <omersarthak11@gmail.com>
2023-06-23 10:41:14 +0530

    remove slide bar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,6 @@ import React, { useContext, useState } from "react";
 import { IoMenuOutline } from "react-icons/io5";
 import { RxCross2 } from "react-icons/rx";
 import { AiOutlineUser } from "react-icons/ai";
-import Slidebar from "./Slidebar";
 import { Link } from "react-router-dom";
 import { LoginContext } from "../context/LoginContext";
 
@@ -69,7 +68,23 @@ function Navbar() {
           </div>
         </div>
       </nav>
-      <Slidebar nav={nav} />
+      {/* Slide Bar */}
+      <div
+          style={{ borderTop: "1px solid gray"}}
+          className={
+            !nav
+              ? "flex fixed top-12 z-10 h-[93.3%] w-full pt-10 pl-10 pr-10 pb-2 bg-black transition-all duration-500 ease-in md:hidden"
+              : "flex fixed left-[-100%] w-0"
+          }
+        >
+          <ul className="w-full text-2xl text-white">
+            {EventLinks.map((EventLinks) => (
+              <Link to={EventLinks.Link} className="">
+                <li style={{ borderBottom: "1px solid gray" }} onClick={()=>{setNav(!nav)}} className="h-10">{EventLinks.Event}</li>
+              </Link>
+            ))}
+          </ul>
+        </div>
     </>
   );
 }
